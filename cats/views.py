6ce9741a8339fd304aa94b4853c8a8d77c7b4298
Tim Gafarov <timu.ryan@yandex.ru>
2025-04-16 06:38:47 +0300

    feat: add CRUD operations for cats

--- cats/views.py
+++ cats/views.py
@@ -2,6 +2,8 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from django.shortcuts import get_object_or_404
+
 from .models import Cat
 from .serializers import CatSerializer
 
@@ -14,6 +16,34 @@ def cat_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     cats = Cat.objects.all()
     serializer = CatSerializer(cats, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def cat_detail(request, pk):
+    try:
+        cat = Cat.objects.get(pk=pk)
+    except Cat.DoesNotExist:
+        return Response({'detail': 'Cat not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = CatSerializer(cat, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if request.method == 'PATCH':
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if request.method == 'DELETE':
+        cat.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    serializer = CatSerializer(cat)
     return Response(serializer.data)
