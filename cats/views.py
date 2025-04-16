e3a33a61483d9b9618bc8ba7c7ab35faef1d31ef
Tim Gafarov <timu.ryan@yandex.ru>
2025-04-16 06:50:05 +0300

    feat: add CBV

--- cats/views.py
+++ cats/views.py
@@ -7,6 +7,18 @@ from django.shortcuts import get_object_or_404
 from .models import Cat
 from .serializers import CatSerializer
 
+from rest_framework import generics
+
+
+class CatList(generics.ListCreateAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+
+class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
 
 @api_view(['GET', 'POST'])
 def cat_list(request):
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from cats.views import cat_list, cat_detail
+from cats.views import CatList, CatDetail
+
 
 urlpatterns = [
-   path('cats/', cat_list),
-   path('cat/<int:pk>/', cat_detail),
+   path('cats/', CatList.as_view()),
+   path('cats/<int:pk>/', CatDetail.as_view()),
 ]
 
 
